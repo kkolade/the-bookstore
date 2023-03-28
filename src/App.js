f2ae6ebc9f6f6918a5a27759f43a289e1e9a377c
Kola Kolade <kolakolade@gmail.com>
2023-03-28 20:50:53 +0100

    Setup nav and link in app compinent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      <div className="App">
+        <nav>
+          <Link to="/">The Great Bookstore</Link>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+              <Link to="/categories">Categories</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Categories />} />
+        </Routes>
+      </div>
     </div>
   );
 }
