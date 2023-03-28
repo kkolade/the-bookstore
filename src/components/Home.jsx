f0c56d84e849eb94f1c61e22326dbc2a0b4547c3
Kola Kolade <kolakolade@gmail.com>
2023-03-28 21:33:25 +0100

    Add form to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import AddBookForm from './AddBookForm';
 import BookList from './BookList';
 
 const Home = () => {
@@ -29,6 +30,7 @@ const Home = () => {
     <div>
       <h2>Book List</h2>
       <BookList books={books} />
+      <AddBookForm />
     </div>
   );
 };
