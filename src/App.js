fc755dff6b08f755f5673bba2f7dfec27840b40a
Kola Kolade <kolakolade@gmail.com>
2023-04-10 19:54:12 +0100

    Changed the bookstore name

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App-container">
       <nav className="App-navbar">
         <Link to="/" className="App-headline">
-          The Great Bookstore
+          The Mindhub Bookstore
         </Link>
         <ul className="App-links">
           <li className="App-link-item">
